b1570e4bd1cbb4bdbca6e6e52d6f444303f4dc5f
rinana7 <sakairinana@gmail.com>
2024-03-26 14:54:43 -1000

    init

---
+++ README.md
@@ -0,0 +1,57 @@
+# How to run
+
+This application is a mock code for Avatar AI using Python. The demo can be viewed on YouTube below.
+
+https://youtu.be/peaXa7jJX4Q
+
+## 1. Install and run VOICEBOX
+
+https://voicevox.hiroshiba.jp/
+
+## 2. Get ChatGPT API Key and set into environment variable
+
+```commandline
+export OPENAI_API_KEY=XXXXXXXXXXXXX
+```
+
+## 3. Set any video file
+
+Please download `Nene.mp4` and `Rin.mp4` below and place them in your local folder.
+https://drive.google.com/drive/folders/1ZMgW_9IsD_AHIlBWX6CNj8fHvbX_VAk1?usp=sharing
+
+Configured the following.
+video is the location of the video file, name is the avatar's name, and speaker is the character's voice parameter. speaker can be changed to other numbers.
+
+```python
+AVATAR_INFO = [
+    {
+        "video": "./Nene.mp4",
+        "name": "ねね",
+        "speaker": 60,
+    },
+    {
+        "video": "./Rin.mp4",
+        "name": "りん",
+        "speaker": 10,
+    }
+]
+```
+
+## 4 Install lib on Mac 
+
+```commandline
+brew install portaudio
+brew install espeak-ng
+```
+
+## 5. Install python packages
+
+```commandline
+pip3 install -r requirements.txt
+```
+
+## 6. Run
+
+```commandline
+python3 main.py
+```
---
+++ main.py
@@ -0,0 +1,122 @@
+import json
+import logging
+import os
+import random
+import sys
+import tempfile
+import threading
+
+import cv2
+import requests
+import simpleaudio
+import speech_recognition
+from openai import OpenAI
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(encoding="utf-8", level=logging.INFO)
+logging.getLogger().addHandler(logging.StreamHandler(sys.stdout))
+
+VOICE_VOX_HOST = "127.0.0.1"
+VOICE_VOX_PORT = 50021
+
+client = OpenAI(api_key=os.environ.get("OPENAI_API_KEY"))
+AVATAR_INFO = [
+    {
+        "video": "./Nene.mp4",
+        "name": "ねね",
+        "speaker": 60,
+    },
+    {
+        "video": "./Rin.mp4",
+        "name": "りん",
+        "speaker": 10,
+    }
+]
+
+AVATAR = AVATAR_INFO[random.randint(0, 1)]
+
+
+def speak(text, speaker=AVATAR['speaker']):
+    logging.info("processing voice vox...")
+    params = (("text", text), ("speaker", speaker))
+    logging.info("getting audio_query...")
+    audio_query = requests.post(
+        f"http://{VOICE_VOX_HOST}:{VOICE_VOX_PORT}/audio_query", params=params
+    )
+
+    logging.info("generating synthesis...")
+    synthesis = requests.post(
+        f"http://{VOICE_VOX_HOST}:{VOICE_VOX_PORT}/synthesis",
+        headers={"Content-Type": "application/json"},
+        params=params,
+        data=json.dumps(audio_query.json()),
+    )
+
+    logging.info("generating audio...")
+    with tempfile.TemporaryDirectory() as tmp:
+        with open(f"{tmp}/audio.wav", "wb") as f:
+            f.write(synthesis.content)
+            wav_obj = simpleaudio.WaveObject.from_wave_file(f"{tmp}/audio.wav")
+            play_obj = wav_obj.play()
+            play_obj.wait_done()
+
+
+def chat_with_chatgpt(prompt, chars=50):
+    logging.info("asking chatGPT...")
+    content = f"{chars}文字程度で教えてもらいたいです。" + prompt
+    logging.info(content)
+    chat_completion = client.chat.completions.create(
+        messages=[{"role": "user", "content": content}],
+        model="gpt-3.5-turbo",
+    )
+    content = chat_completion.choices[0].message.content
+    speak(content)
+
+
+def recognize_voice():
+    recognizer = speech_recognition.Recognizer()
+    microphone = speech_recognition.Microphone()
+    with microphone as source:
+        logging.info("recognizing voice...")
+        # recognizer.adjust_for_ambient_noise(source)
+        audio = recognizer.listen(source, timeout=2, phrase_time_limit=5)
+    try:
+        logging.info("recognizing by google...")
+        result = recognizer.recognize_google(audio, language="ja-JP")
+        if not result:
+            speak("何かおっしゃってください。")
+        logger.info(result)
+        chat_with_chatgpt(result)
+    except speech_recognition.UnknownValueError as e:
+        logger.error(e)
+        speak("すいません。質問の意味が分かりませんでした。")
+    except speech_recognition.RequestError as e:
+        logger.error(e)
+        speak("すいません。質問の意味が分かりませんでした。")
+
+
+def display_video():
+    cap = cv2.VideoCapture(AVATAR['video'])
+    if not cap.isOpened():
+        logger.error("cannot open")
+
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            break
+        cv2.imshow("Video", frame)
+        if cv2.waitKey(25) & 0xFF == ord("q"):
+            break
+
+
+def run_ai():
+    speak(f"こんにちは。{AVATAR['name']}です。何かご用ですか？")
+    while True:
+        recognize_voice()
+        speak("ほかにご用はございますか？")
+
+
+if __name__ == "__main__":
+    thread = threading.Thread(target=run_ai)
+    thread.start()
+    display_video()
